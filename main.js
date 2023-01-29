fe2609d4657342650aaf1a00f9704f0033570954
unknown <quiensoyanna99@gmail.com>
2023-01-28 19:46:48 -0500

    Add side menu

--- main.js
+++ main.js
@@ -1,3 +1,12 @@
-const burgerBtn = document.querySelector(".burger-btn")
-burgerBtn.addEventListener
+const burgerBtn = document.querySelector(".burger-btn");
+const sideMenu = document.querySelector(".side__menu");
+const closeBtn = document.querySelector(".burger-btn-close");
 
+burgerBtn.addEventListener("click", () => {
+  sideMenu.classList.add("active");
+});
+
+closeBtn.addEventListener("click", () => {
+  console.log("click");
+  sideMenu.classList.remove("active");
+});
